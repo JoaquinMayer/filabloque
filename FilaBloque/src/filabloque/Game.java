1b54d91844419042c89953352fa8a3ea817664ec
Joaquin Mayer <joaquin@vexels.com>
2021-05-16 00:24:47 -0300

    Remove comments

--- FilaBloque/src/filabloque/Game.java
+++ FilaBloque/src/filabloque/Game.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package filabloque;
 
 import java.io.File;
@@ -16,7 +11,8 @@ import javax.sound.sampled.DataLine;
 
 /**
  *
- * @author joaquin
+ * @authors Agustin Tosar & Joaquin Mayer
+ * 
  */
 public class Game {
 
