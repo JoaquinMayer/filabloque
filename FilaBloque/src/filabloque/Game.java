f4de56a7268ec05f2a79b22d78be1b03c70ecc88
Joaquin Mayer <joaquin@vexels.com>
2021-05-16 00:07:47 -0300

    Change variable names

--- FilaBloque/src/filabloque/Game.java
+++ FilaBloque/src/filabloque/Game.java
@@ -27,9 +27,9 @@ public class Game {
     private boolean[][] completedDesigns;
     private boolean finishedGame;
     private boolean exitGame = false;
-    private String ANSI_RESET = "\u001B[0m";
-    private String ANSI_RED = "\u001B[31m";
-    private String ANSI_BLUE = "\u001B[34m";
+    private final String RESET_COLOR = "\u001B[0m";
+    private final String RED_COLOR = "\u001B[31m";
+    private final String BLUE_COLOR = "\u001B[34m";
 
     public Game(Player player1, Player player2) {
         this.player1 = player1;
@@ -196,10 +196,10 @@ public class Game {
 
         if (this.playerTurn == 1) {
             actualPlayerName = this.player1.getName();
-            System.out.print("Es el turno de " + ANSI_RED + actualPlayerName + ANSI_RESET);
+            System.out.print("Es el turno de " + RED_COLOR + actualPlayerName + RESET_COLOR);
         } else {
             actualPlayerName = this.player2.getName();
-            System.out.print("Es el turno de " + ANSI_BLUE + actualPlayerName + ANSI_RESET);
+            System.out.print("Es el turno de " + BLUE_COLOR + actualPlayerName + RESET_COLOR);
         }
 
         System.out.println();
